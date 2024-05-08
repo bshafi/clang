6d23026d4c5ed0ffba692c589024463f50686d54
bshafi <bshaf001@ucr.edu>
2024-05-07 20:28:55 -0700

    EVEN BETTER

--- build_wasm.sh
+++ build_wasm.sh
@@ -1,3 +1,3 @@
 
-echo 'int printf(const char *, ...); int main(){printf("hello world!\\n");}' |  wasmtime  --dir=. clang.wasm -cc1 -triple wasm32-unknown-wasi -isysroot /sys -internal-isystem /sys/include -emit-obj -o ./example.o -
+echo 'int printf(const char *, ...); int main(){printf("hello world!\\n");}' |  wasmtime  --dir=. clang.wasm -cc1 -triple wasm32-unknown-wasi -isysroot ./sysroot -internal-isystem ./sysroot/include -emit-obj -o ./example.o -
 wasmtime --dir=. --dir=./sysroot/lib wasm-ld.wasm -L./sysroot/lib/wasm32-wasi ./sysroot/lib/wasm32-wasi/crt1.o ./example.o -lc -o ./example.wasm
